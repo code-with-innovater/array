b1c23962e251e3187853f586e5b6562aa242610d
code-with-innovater <st168035@gmail.com>
2024-10-30 23:15:34 +0530

    kth largest array

---
+++ kthlargest.java
@@ -0,0 +1,19 @@
+import java.util.PriorityQueue;
+
+public class kthlargest {
+    public static int findKthLargest(int[] nums, int k) {
+        PriorityQueue<Integer> minHeap = new PriorityQueue<>();
+        for (int i = 0; i < nums.length; i++) {
+            minHeap.add(nums[i]);  
+            if (minHeap.size() > k) {  
+                minHeap.poll();  
+            }
+        }
+        return minHeap.peek();
+    }
+    public static void main(String[] args) {
+        int[] nums = {3, 2, 1, 5, 6, 4};
+        int k = 2;
+        System.out.println(findKthLargest(nums, k));  
+    } 
+}
